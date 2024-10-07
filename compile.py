6e0f4e086b4b64ff9612cb89b71ce3f8255d6cfb
rponeawa <rponeawa@qq.com>
2024-10-08 01:11:10 +0800

    gui support

--- compile.py
+++ compile.py
@@ -3,15 +3,12 @@ from sys import platform
 import PyInstaller.__main__
 
 args = [
-    'cli_app.py',
+    'gui_app.py',
     '--hidden-import=zeroconf',
     '--hidden-import=zeroconf._utils.ipaddress',
     '--hidden-import=zeroconf._handlers.answers',
-    '--onedir'
+    '--onedir',
+    '--windowed'
 ]
-
-if platform == "darwin":
-    # add --windowed arg for macOS
-    args.append('--windowed')
 
 PyInstaller.__main__.run(args)
