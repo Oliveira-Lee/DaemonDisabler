7485230892be83b267ee84113ab9215bf4cbb09c
rponeawa <rponeawa@qq.com>
2024-10-09 01:00:25 +0800

    add credit

--- gui_app.py
+++ gui_app.py
@@ -1,6 +1,9 @@
 from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtSvg import QSvgWidget
 import qdarktheme
 import platform
+
+import resources_rc
 
 from exploit.restore import restore_files, FileToRestore
 from devicemanagement.constants import Device
@@ -21,7 +24,7 @@ class App(QtWidgets.QWidget):
         self.language_pack = {
             "en": {
                 "title": "thermalmonitordDisabler",
-                "modified_by": "Modified by rponeawa from LeminLimez's Nugget",
+                "modified_by": "Modified by rponeawa from LeminLimez's Nugget.\nFree tool. If you purchased it, please report the seller.",
                 "backup_warning": "Please back up your device before using!",
                 "connect_prompt": "Please connect your device and try again!",
                 "connected": "Connected to",
@@ -44,7 +47,7 @@ class App(QtWidgets.QWidget):
             },
             "zh": {
                 "title": "温控禁用工具",
-                "modified_by": "由 rponeawa 基于 LeminLimez 的 Nugget 修改",
+                "modified_by": "由 rponeawa 基于 LeminLimez 的 Nugget 修改。\n免费工具，若您是购买而来，请举报卖家。",
                 "backup_warning": "使用前请备份您的设备！",
                 "connect_prompt": "请连接设备并重试！",
                 "connected": "已连接到",
@@ -85,6 +88,26 @@ class App(QtWidgets.QWidget):
         self.modified_by_label = QtWidgets.QLabel(self.language_pack[self.language]["modified_by"])
         self.layout.addWidget(self.modified_by_label)
 
+        self.icon_layout = QtWidgets.QHBoxLayout()
+
+        self.icon_layout.setAlignment(QtCore.Qt.AlignLeft)
+        self.icon_layout.setSpacing(10)
+
+        self.github_icon = QSvgWidget(":/brand-github.svg")
+        self.github_icon.setFixedSize(24, 24)
+        self.github_icon.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.github_icon.mouseReleaseEvent = lambda event: self.open_link("https://github.com/rponeawa/thermalmonitordDisabler")
+
+        self.bilibili_icon = QSvgWidget(":/brand-bilibili.svg")
+        self.bilibili_icon.setFixedSize(24, 24)
+        self.bilibili_icon.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.bilibili_icon.mouseReleaseEvent = lambda event: self.open_link("https://space.bilibili.com/332095459")
+
+        self.icon_layout.addWidget(self.github_icon)
+        self.icon_layout.addWidget(self.bilibili_icon)
+
+        self.layout.addLayout(self.icon_layout)
+
         self.device_info = QtWidgets.QLabel(self.language_pack[self.language]["backup_warning"])
         self.layout.addWidget(self.device_info)
 
@@ -111,6 +134,12 @@ class App(QtWidgets.QWidget):
 
         self.setLayout(self.layout)
         self.show()
+
+    def open_link(self, url):
+        try:
+            QtGui.QDesktopServices.openUrl(QtCore.QUrl(url))
+        except Exception as e:
+            print(f"Error opening link {url}: {str(e)}")
 
     def get_device_info(self):
         connected_devices = usbmux.list_devices()
