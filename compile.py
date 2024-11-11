3e364f28a05fdc82773f7a0b3c8af92d031c4d14
ringojuice <ringojuice@outlook.com>
2024-11-11 23:25:45 +0800

    feat: 在可用时生成 universal2 binary

--- compile.py
+++ compile.py
@@ -1,7 +1,12 @@
 from sys import platform
 import shutil
-
+import subprocess
 import PyInstaller.__main__
+
+def universal2():
+    python_path = shutil.which("python3")
+    arch = subprocess.run(['file', python_path], capture_output=True, text=True)
+    return 'arm64' in arch.stdout and 'x86_64' in arch.stdout
 
 args = [
     'gui_app.py',
@@ -10,8 +15,13 @@ args = [
     '--hidden-import=zeroconf._handlers.answers',
     '--onedir',
     '--name=DaemonDisabler',
-    '--windowed'
+    '--windowed',
+    '--noconfirm'
 ]
+
+if platform == "darwin":
+    if universal2():
+        args.append('--target-arch=universal2')
 
 PyInstaller.__main__.run(args)
 
